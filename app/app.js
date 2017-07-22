a80abc4200c6f937b020a7b987393530983ca98b
Eric Tao <thr19910704@sina.com>
2017-07-23 00:10:53 +0800

    组件添加
    ajax组件，倒计时组件

--- app/app.js
+++ app/app.js
@@ -2,32 +2,73 @@
 
 import React from 'react' ;
 import ReactDOM from 'react-dom' ;
+import Ajax from './modules/ajax.js';
 import "./style.less"
 
+// 倒计时组件
+class Count extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render(){
+      console.log(this.props.count);
+      return (
+        <span>{this.props.count<=0?"发送验证码":`请耐心等待${this.props.count}s`}</span>
+      )
+  }
+
+}
+
+// 表单组件
 class Form extends React.Component {
 
     constructor(props) {
       super(props);
       this.state={
-        value:""
+        value:"",
+        btnText:"发送验证码",
+        count:0
       };
       this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e) {
-      this.setState({value: event.target.value});
+      this.setState({value: e.target.value});
     }
 
     handleSubmit(e){
       e.preventDefault()
-      console.log("submit");
+
+      let phoneReg = /^1[34578]\d{9}$/;
+
+      if(!phoneReg.test(this.state.value))
+       {
+          alert('请输入有效的手机号码！');
+          return
+       } else {
+          //  alert('手机号可以了！');
+          //  Ajax.post("/getCode",{phone:this.state.value},function(res){
+          //    console.log(res);
+          //  })
+           this.setState({count: 5});
+           this.timer = setInterval(()=>{
+             this.setState({count: this.state.count-1});
+             if(this.state.count<1){
+               clearInterval(this.timer)
+             }
+           },1000)
+       }
+
     }
 
     render(){
       return (
         <form onSubmit={this.handleSubmit}>
           <input type="text" value={this.state.value} onChange={this.handleChange} />
-          <button type="submit">发送验证码</button>
+          <button type="submit">
+            <Count count={this.state.count}></Count>
+          </button>
         </form>)
     }
 }
