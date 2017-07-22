6cff6483d0967249e2238bcb7e0c75a2980ab6fb
Eric Tao <thr19910704@sina.com>
2017-07-22 22:45:13 +0800

    dom
    设计dom结构

--- app/app.js
+++ app/app.js
@@ -4,8 +4,36 @@ import React from 'react' ;
 import ReactDOM from 'react-dom' ;
 import "./style.less"
 
+class Form extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state={
+        value:""
+      };
+      this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+      this.setState({value: event.target.value});
+    }
+
+    handleSubmit(e){
+      e.preventDefault()
+      console.log("submit");
+    }
+
+    render(){
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <button type="submit">发送验证码</button>
+        </form>)
+    }
+}
+
 ReactDOM.render(
-  <div>Hello, world!</div>,
+  <Form></Form>,
   document.getElementById('app')
 );
 
