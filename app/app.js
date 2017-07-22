544748a24e8e0fe488e5d245784e3f41845ee838
Eric Tao <thr19910704@sina.com>
2017-07-22 22:06:31 +0800

    脚手架
    npm init ……

---
+++ app/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react' ;
+import ReactDOM from 'react-dom' ;
+
+
+ReactDOM.render(
+  <div>Hello, world!</div>,
+  document.getElementById('app')
+);
+
+
+console.log("ok");
